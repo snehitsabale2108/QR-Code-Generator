e91b92438c2bad07f864cfd2712fe6f0a1aaa68d
Snehit Sabale <158484294+snehitsabale2108@users.noreply.github.com>
2026-05-08 15:12:48 +0530

    Add files via upload

---
+++ QR Code Generator.py
@@ -0,0 +1,6 @@
+import qrcode
+a = qrcode.make("https://www.linkedin.com/in/snehit-sabale-09916b2a3/")
+a.save("Snehit LinkedIN Profile.png")
+
+a = qrcode.make("https://github.com/snehitsabale2108")
+a.save("Snehit Github.png")
